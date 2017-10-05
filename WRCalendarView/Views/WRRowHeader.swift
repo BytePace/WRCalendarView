75310fc2e6f41a09793fbef4f248826980c3bca1
Ruslan Musagitov <ruslan.musagitov@gmail.com>
2017-10-06 00:41:48 +0600

    date format changed

--- WRCalendarView/Views/WRRowHeader.swift
+++ WRCalendarView/Views/WRRowHeader.swift
@@ -14,7 +14,7 @@ class WRRowHeader: UICollectionReusableView {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        dateFormatter.dateFormat = "HH"
+        dateFormatter.dateFormat = "H"
     }
 
     var date: Date? {
