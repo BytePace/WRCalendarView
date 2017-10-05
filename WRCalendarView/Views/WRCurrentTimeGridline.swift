58524731457266d2e2d62bf0809e21e1ad6dc4c0
Ruslan Musagitov <ruslan.musagitov@gmail.com>
2017-10-05 15:59:15 +0600

    added circles on start and end of current time gridline

--- WRCalendarView/Views/WRCurrentTimeGridline.swift
+++ WRCalendarView/Views/WRCurrentTimeGridline.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class WRCurrentTimeGridline: UICollectionReusableView {
+    let circleView1 = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: 4))
+    let circleView2 = UIView(frame: CGRect(x: 0, y: 0, width: 4, height: 4))
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor(hexString: "fd3935")
+        backgroundColor = UIColor(hexString: "5E97E7")
+        addSubview(circleView1)
+        addSubview(circleView2)
+        circleView1.layer.cornerRadius = 2
+        circleView2.layer.cornerRadius = 2
+        
+        circleView1.layer.masksToBounds = true
+        circleView2.layer.masksToBounds = true
+        
+        circleView1.backgroundColor = backgroundColor
+        circleView2.backgroundColor = backgroundColor
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        var f = circleView2.bounds
+        f.origin.y = -circleView1.frame.size.height / 2
+        f.origin.x = -circleView1.frame.size.width / 2
+        circleView1.frame = f
+        f.origin.x = bounds.size.width - circleView2.frame.size.width / 2
+        circleView2.frame = f
     }
     
     required init?(coder aDecoder: NSCoder) {
